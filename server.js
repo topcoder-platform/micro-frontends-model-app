17ce7f0b3e86692374821b53a13af65f748e17cf
urwithat <atif.siddiqui2@topcoder.com>
2021-01-27 16:05:15 +0530

    Fix app location

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-app.use('/model',
+app.use('/model-app',
   express.static("./dist", {
     setHeaders: function setHeaders(res) {
       res.header("Access-Control-Allow-Origin", "*");
