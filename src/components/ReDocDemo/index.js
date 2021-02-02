0f8eb907f6c910a58b89846f434a57abdefe73f6
urwithat <atif.siddiqui2@topcoder.com>
2021-02-02 14:35:39 +0530

    redoc disable sidebar

--- src/components/ReDocDemo/index.js
+++ src/components/ReDocDemo/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { RedocStandalone } from 'redoc';
+import {
+  disableSidebarForRoute,
+  enableSidebarForRoute,
+} from "@topcoder/micro-frontends-navbar-app";
+import "./styles.module.scss";
+
+const COMPONENT_ROUTE = "/model/redoc/*";
 
 const ReDocDemo = () => {
 
+  useLayoutEffect(() => {
+    disableSidebarForRoute(COMPONENT_ROUTE);
+  }, []);
+
   return (
     <div>
-      <meta http-equiv="Content-Security-Policy" content="worker-src 'self' blob:"></meta>
+      <div styleName="floating-sidebar">
+        Sidebar :   
+        <button onClick={() => enableSidebarForRoute(COMPONENT_ROUTE)}>
+          Enable
+        </button>
+        <button onClick={() => disableSidebarForRoute(COMPONENT_ROUTE)}>
+          Disable
+        </button>
+      </div>
       <RedocStandalone
         specUrl="https://petstore.swagger.io/v2/swagger.json"
       />
